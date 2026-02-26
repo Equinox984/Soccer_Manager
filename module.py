a6aaaf4d633a51f8fcd163c265971d37fd0959a8
Equinox984 <lopezkris984@proton.me>
2026-02-26 11:06:02 -0600

    Add soccer teams to the leagues dictionary

--- module.py
+++ module.py
@@ -1,6 +1,6 @@
 """Module for Soccer Manager Engine"""
 
-soccer_leagues = {
+soccer_teams = {
     "Honduran National Professional Football League (First Division)": [
         "Real España",
         "Motagua",
@@ -14,7 +14,50 @@ soccer_leagues = {
         "Lobos UPNFM",
         "Club Deportivo Génesis Policía Nacional",
     ],
-    "Honduran Second Division (Ascenso League)": ["Platense", ""],
-    "Honduran Mayor Football League (Third Division)": "Troya F.C.",
-    "Intermediate Soccer League": "Parrillas One",
+    "Honduran Second Division (Ascenso League)": [
+        # Group A
+        "Boca Juniors",
+        "Vida",
+        "Juventus",
+        "Social Sol",
+        "Sampdoria",
+        "Real Sociedad",
+        "Roma F.C.",
+        # Group B
+        "Honduras Progreso",
+        "Atlético Junior",
+        "Tela F.C.",
+        "F.C. Santa Rosa",
+        "Oro Verde F.C.",
+        "Estrella F.C.",
+        # Group C
+        "CD Choloma",
+        "Parrillas One",
+        "Leones HT6",
+        "C.D. Brasilia",
+        "Pumas F.C.",
+        "Lone F.C.",
+        # Group D
+        "San Juan Huracán",
+        "Olimpia Occidental",
+        "Santo Domingo Savio",
+        "Cuervos",
+        "San Juan Gualjoco",
+        "Real Juventud",
+        # Group E
+        "Atlético Independiente",
+        "Real Tegus",
+        "AFFI Academia",
+        "CD Pirata",
+        "FAS",
+        "Inter",
+        "Policía Nacional",
+        # Group F
+        "Hondupino",
+        "Estrella Roja",
+        "Gimnástico",
+        "Arsenal SAO",
+        "San Rafael",
+        "Meluca F.C.",
+    ],
 }
