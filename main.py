d42ec109cf5f0b1f540a28dc5f54732a7b345327
Equinox984 <lopezkris984@proton.me>
2026-02-25 21:24:26 -0600

    Revise project description and menu comments
    Updated project description and menu structure for Soccer Manager Engine.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-"""Nexus Finance Engine"""
+"""Soccer Manager Engine"""
+"""
+The purpose of this project is to create a tool that allows soccer enthusiasts to
+have all the information from soccer teams, to soccer leagues from Honduras.
+"""
+# Create a function that shows the Soccer Manager Menu
+# - Submenu 1: If the user selects 1, we will have a Menu with all the Soccer Leagues
+# and their teams.
+# It will also have a Finder, where we will have information from every team. A mini
+# Wikipedia basically.
+# - Submenu 2: If the user selects 2, we will show with this function every match
+# available with their dates and stadiums.
+# - Submenu 3: If the user selects 3, we will have a Trivia Minigame, where you need
+# to select the correct option from fun facts of
+# every honduran team. Then we will show the results in the display.
 
-
-# Lists & Modules
-
-# Welcome & Main Menu
+# === MENUS ===
+# Menu 1: Soccer Leagues -> Matches, Teams. Finder.
+# Menu 2: Matches with Dates & Stadium Names Menu
+# Menu 3: Trivia Minigame
