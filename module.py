5cc943acecd998d39ca9f0411aa724a2e9310616
Equinox984 <lopezkris984@proton.me>
2026-02-26 10:27:39 -0600

    Add soccer leagues data structure

---
+++ module.py
@@ -0,0 +1,20 @@
+"""Module for Soccer Manager Engine"""
+
+soccer_leagues = {
+    "Honduran National Professional Football League (First Division)": [
+        "Real España",
+        "Motagua",
+        "Olimpia",
+        "Victoria",
+        "Olancho F.C.",
+        "Maratón",
+        "Platense",
+        "Club Deportivo Choloma",
+        "Juticalpa",
+        "Lobos UPNFM",
+        "Club Deportivo Génesis Policía Nacional",
+    ],
+    "Honduran Second Division (Ascenso League)": ["Platense", ""],
+    "Honduran Mayor Football League (Third Division)": "Troya F.C.",
+    "Intermediate Soccer League": "Parrillas One",
+}
