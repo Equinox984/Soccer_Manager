20825e9f7d236db9d2407a1250c572a16e463b1f
Equinox984 <lopezkris984@proton.me>
2026-03-15 21:36:32 -0600

    Add soccer trivia questions to trivia_module

---
+++ trivia_module.py
@@ -0,0 +1,29 @@
+"""Trivia Module"""
+
+soccer_trivia = [
+    {
+        "question": "¿Qué equipo es conocido mundialmente como 'El Viejo León'?",
+        "options": ["Motagua", "Olimpia", "Maratón", "Real España"],
+        "answer": "Olimpia",
+    },
+    {
+        "question": "¿En qué ciudad juega tradicionalmente el Real España?",
+        "options": ["Tegucigalpa", "La Ceiba", "San Pedro Sula", "Choluteca"],
+        "answer": "San Pedro Sula",
+    },
+    {
+        "question": "¿Cuál de estos equipos pertenece a la ciudad de La Ceiba?",
+        "options": ["Victoria", "Platense", "Juticalpa", "Olancho F.C."],
+        "answer": "Victoria",
+    },
+    {
+        "question": "¿Cómo se le conoce popularmente al clásico entre Olimpia y Motagua?",
+        "options": [
+            "Clásico de las M's",
+            "Clásico Sampedrano",
+            "Clásico Capitalino",
+            "Clásico del Ceibeño",
+        ],
+        "answer": "Clásico Capitalino",
+    },
+]
